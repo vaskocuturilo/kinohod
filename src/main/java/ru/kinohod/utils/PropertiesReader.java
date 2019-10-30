5b4dd673e1b0e80abbbb9d8a90b15155da3d7d6c
vaskocuturilo@rambler.ru <wolfstonik54321>
2019-10-30 23:37:32 +0200

    Feat : PropertiesReader : Added final and package-info.java.
    Commit at 30/10/2019

--- src/main/java/ru/kinohod/utils/PropertiesReader.java
+++ src/main/java/ru/kinohod/utils/PropertiesReader.java
@@ -10,7 +10,7 @@ import java.util.Properties;
 /**
  * The class Properties reader.
  */
-public class PropertiesReader {
+public final class PropertiesReader {
     /**
      * Constant LOGGER.
      */
