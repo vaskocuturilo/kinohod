2d67ff998b431e8ff6e9de9c3553e29b429798f1
Vasko <vaskocuturilo@rambler.ru`>
2019-11-27 13:11:18 +0300

    Fix : Environment : Fix method to isCheckOperatingSystem.
    Commit at 27/11/2019

--- src/main/java/ru/kinohod/environment/Environment.java
+++ src/main/java/ru/kinohod/environment/Environment.java
@@ -9,13 +9,13 @@ import java.util.Locale;
 public class Environment {
 
     /**
-     * Method isCheckOSSystem.
+     * Method isCheckOperatingSystem.
      * If method return true, that is automation scripts to run on OS Unix or Linux.
      * If method return false, that is automation scripts to run on OS Windows or Mac.
      *
      * @return the boolean.
      */
-    public static boolean isCheckOperationSystem() {
+    public static boolean isCheckOperatingSystem() {
 
         String systemName = System.getProperty("os.name").toLowerCase(Locale.ROOT);
         return (systemName.contains("nix") || systemName.contains("nux"));
